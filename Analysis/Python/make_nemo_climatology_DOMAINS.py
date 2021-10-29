c11fe87c3dd6d4e81309cf3afc976f57e667a949
Jason T Holt <jholt@livljobs7.nerc-liv.ac.uk>
2021-10-29 16:59:58 +0100

    updates

--- Analysis/Python/make_nemo_climatology_DOMAINS.py
+++ Analysis/Python/make_nemo_climatology_DOMAINS.py
@@ -12,9 +12,19 @@ import coast
 import matplotlib.pylab as plt
 import make_nemo_climatology as mc
 import pickle as pkl
-
 domain_datapath='/work/jholt/JASMIN//SENEMO/NOTIDE/'
 domain_outpath='/projectsa/NEMO/jholt/SE-NEMO/ASSESSMENT/'
+domain_path='/projectsa/NEMO/jholt/SE-NEMO/INPUTS/'
+
+domain_datapath='/gws/nopw/j04/class_vol2/senemo/cwilso01/senemo/EXP_REF_NOTIDE/means/monthly/'
+domain_outpath='/home/users/jholt/work/SENEMO/'
+domain_path='/gws/nopw/j04/class_vol2/senemo/cwilso01/senemo/EXP_REF_NOTIDE/'
+DOMNAM='ORCA025-SE-NEMO'
+
+fn_nemo_dom=domain_path+'domcfg_eORCA025_v2.nc'
+fn_config_t_grid='/vkamino/work/jholt/Git/SE-NEMO/Analysis/Config/senemo_grid_t.json'
+fn_nemo_dat=domain_datapath+'/SENEMO_1m_19800101_19801231_grid_T_1980*-1980*.nc'
+
 DOMNAM='ORCA025-SE-NEMO'
   
 fn_nemo_dom=domain_path+'domcfg_eORCA025_v2.nc'
