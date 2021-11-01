d12cf5ff2c0d27c10c0f46de4e4af54321982ea4
Jason T Holt <jholt@livljobs7.nerc-liv.ac.uk>
2021-11-01 17:24:20 +0000

    updates

--- Analysis/Python/make_nemo_climatology.py
+++ Analysis/Python/make_nemo_climatology.py
@@ -5,6 +5,7 @@ Created on Thu Sep  5 16:27:35 2019
 
 @author: jholt
 """
+import socket
 import numpy as np
 import coast
 Zmax=200
@@ -23,6 +24,7 @@ def calc_pea(nemo,Zd_mask):
      nemo.construct_density(CT_AS=True,rhobar=True,Zd_mask=Zd_mask,pot_dens=True)
  
  rho=nemo.dataset.variables['density'].values
+ rho[np.isnan(rho)]=0
  rhobar=nemo.dataset.variables['density_bar'].values
  
  z_axis=0
@@ -101,27 +103,71 @@ def make_climatology(nemo,nemo_w,DOMNAM,domain_outpath):
  SSTy=np.zeros((12,ny,nx))
  SSSy=np.zeros((12,ny,nx))
  PEAy=np.zeros((12,ny,nx))
- NBTy=np.zeros((12,ny,nx))
+ #NBTy=np.zeros((12,ny,nx))
  
 #%%  
  tmp=nemo.dataset.variables['temperature']
  sal=nemo.dataset.variables['salinity']
  PEA=calc_pea(nemo,Zd_mask)
- NBT=tmp[:,kmax[:,:],:,:]
+ 
+ #need to find efficient method for bottom temperature
+ #NBT=np.zeros((nt,ny,nx))
+ #for it in range(nt): 
+ #    NBT[it,:,:]=np.reshape(tmp[it,:,:,:].values.ravel()[Ikmax],(ny,nx))
+     
  SST=tmp[:,0,:,:]
- SST=sal[:,0,:,:]
+ SSS=sal[:,0,:,:]
  for im in range(12):
    print('Month',im)
-   it=np.arange[im,nt,12].astype(int)
+   it=np.arange(im,nt,12).astype(int)
    SSTy[im,:,:]=np.mean(SST[it,:,:],axis=0)
    SSSy[im,:,:]=np.mean(SSS[it,:,:],axis=0)
    PEAy[im,:,:]=np.mean(PEA[it,:,:],axis=0)
-   NBTy[im,:,:]=np.mean(NBT[it,:,:],axis=0)
+  # NBTy[im,:,:]=np.mean(NBT[it,:,:],axis=0)
 
- return SSTy,SSSy,PEAy,NBTy
+ return SSTy,SSSy,PEAy #,NBTy
+#%%
+def NEMO_FileNames(dpath,runtype,ystart,ystop):
+#produce a list of nemo filenames
+    names=[]    
+    if runtype== 'SENEMO':
+     for iy in range(ystart,ystop+1):
+        for im in range(1,12+1):    
+            MNTH=str(im);
+            if im<10:
+                 MNTH='0'+ MNTH
+            YEAR=str(iy)
+            new_name="{0}/SENEMO_1m_{1}0101_{1}1231_grid_T_{1}{2}-{1}{2}.nc".format(dpath,YEAR,MNTH)
+            names.append(new_name)
+    return names         
+#%%    
+if 'livljobs' in socket.gethostname(): #NOC-liverpool
+    domain_datapath='/work/jholt/JASMIN//SENEMO/NOTIDE/'
+    domain_outpath='/projectsa/NEMO/jholt/SE-NEMO/ASSESSMENT/'
+    domain_path='/projectsa/NEMO/jholt/SE-NEMO/INPUTS/'
+else: #JASMIN
+    domain_datapath='/gws/nopw/j04/class_vol2/senemo/cwilso01/senemo/EXP_REF_NOTIDE/means/monthly/'
+    domain_outpath='/home/users/jholt/work/SENEMO/'
+    domain_path='/gws/nopw/j04/class_vol2/senemo/cwilso01/senemo/EXP_REF_NOTIDE/'
 
+fn_nemo_dom=domain_path+'domcfg_eORCA025_v2.nc'
+fn_config_t_grid='../Config/senemo_grid_t.json'
+fn_nemo_dat=domain_datapath+'/SENEMO_1m_19800101_19801231_grid_T_1980*-1980*.nc'
+
+#make list of filenames
+ystart=1980
+ystop=2009
+fn_nemo_dat= NEMO_FileNames(domain_datapath,'SENEMO',ystart,ystop)
+  
+fn_nemo_dom=domain_path+'domcfg_eORCA025_v2.nc'
+fn_config_t_grid='/vkamino/work/jholt/Git/SE-NEMO/Analysis/Config/senemo_grid_t.json'    
+#fn_nemo_dat=domain_datapath+'/SENEMO_1m_19800101_19801231_grid_T_1980*-1980*.nc'
+#input datasets
+nemo = coast.Gridded(fn_data= fn_nemo_dat, fn_domain = fn_nemo_dom, config=fn_config_t_grid,multiple=True)
+nemo_w=coast.Gridded(fn_domain = fn_nemo_dom ,config='../Config/example_nemo_grid_w.json')
+DOMNAM='ORCA025-SE-NEMO'
+print('running')
+#%%
+SSTy,SSSy, PEAy  = make_climatology(nemo,nemo_w,DOMNAM,domain_outpath)
 
  
- 
- 
-
