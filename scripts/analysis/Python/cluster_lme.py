f36a3c4ae19a26c9b441e0766269e6cfd4c618da
jasontempestholt <42639421+jasontempestholt@users.noreply.github.com>
2022-12-09 17:59:56 +0000

    Clustering LME's

--- scripts/analysis/Python/cluster_lme.py
+++ scripts/analysis/Python/cluster_lme.py
@@ -5,43 +5,55 @@ import pandas as pd
 import sys
 sys.path.insert(0, 'C:\\Users\\Jason.Goliath\\Documents\\GitHub\\COAsT\\')
 import coast
-
+import scipy.io
 LME_Data=np.load('../Data/LME_gridinfo_V4.npz')
-
+a=scipy.io.loadmat('../Data/ORCA025_ROAM_GLB_LMEmaskV4.mat')
+LME_mask=a['LME_mask'][:,:].T
 LME_Clusters='../Data/LME_Clusters.csv'
 clusters = pd.read_csv(LME_Clusters)
 clusters = clusters.to_numpy()
 cluster_names=clusters[:,0]
 LME_numbers=clusters[:,1:]
 Clusters={}
-for iname,name in enumerate(cluster_names):
-    lmes=LME_numbers[iname,:].astype('float')
-    lmes=lmes[np.isfinite(lmes)].astype(int)
-    Clusters[name]={}
-    Clusters[name]['LMEs']=lmes
-
-    Clusters[name]['limits']=np.array([np.min(LME_Data['x_min'][lmes-1]),
-                                     np.max(LME_Data['x_max'][lmes-1]),
-                                     np.min(LME_Data['y_min'][lmes-1]),
-                                     np.max(LME_Data['y_max'][lmes-1])])
-
-
 
 bathyname='../Data/eORCA025_bathy_meter.nc'
 config='example_nemo_grid_t.json'
 bathy=coast.Gridded(fn_data=bathyname,config=config)
 D=np.ma.masked_where(bathy.dataset.Bathymetry.values==0,bathy.dataset.Bathymetry.values)
+lon=bathy.dataset.longitude
+lat=bathy.dataset.latitude
+
+
 nx=D.shape[1]
+J_offset=186 #account for extra rows in eORCA if data is made for normal ORCA
+
+for iname,name in enumerate(cluster_names):
+    lmes=LME_numbers[iname,:].astype('float')
+    lmes=lmes[np.isfinite(lmes)].astype(int)
+    Clusters[name]={}
+    Clusters[name]['LMEs']=lmes
+    lims=[99999,-99999,99999,-99999]
+    for lme in lmes:
+        a=np.where(LME_mask==lme) 
+
+   
+        lims=np.array([np.min([lims[0],np.min(a[1])]),
+               np.max([lims[1],np.max(a[1])]),
+               np.min([lims[2],np.min(a[0])]),
+               np.max([lims[3],np.max(a[0])])    
+             ])
+
+    Clusters[name]['limits']=lims+[0,0,J_offset,J_offset]
+
 plt.pcolormesh(D)
 for name in cluster_names:
     lims=Clusters[name]['limits']
-    j, i, _ = bathy.find_j_i_list(lon=[lims[0], lims[1], lims[1], lims[0]], lat=[lims[2], lims[2],lims[3], lims[3] ])
 
-    imin = min(i)
-    imax = max(i)
-    jmin = min(j)
-    jmax = max(j)
-    if i[0] < i[1]:
+    imin = lims[0]
+    imax = lims[1]
+    jmin = lims[2]
+    jmax = lims[3]
+    if name !='S Asia':
       plt.plot([imin,imax,imax,imin,imin],[jmin,jmin,jmax,jmax,jmin])
     else:
       print(name)
