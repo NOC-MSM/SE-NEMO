018bd9c4e774ee7c42984c1456fe06da41ef2e38
jasontempestholt <42639421+jasontempestholt@users.noreply.github.com>
2022-12-12 17:19:16 +0000

    Cluster LMEs

--- scripts/analysis/Python/cluster_lme.py
+++ scripts/analysis/Python/cluster_lme.py
@@ -10,6 +10,8 @@ LME_Data=np.load('../Data/LME_gridinfo_V4.npz')
 a=scipy.io.loadmat('../Data/ORCA025_ROAM_GLB_LMEmaskV4.mat')
 LME_mask=a['LME_mask'][:,:].T
 LME_Clusters='../Data/LME_Clusters.csv'
+LME_Clusters_out='../Data/LME_Clusters_eORCA025.csv'
+
 clusters = pd.read_csv(LME_Clusters)
 clusters = clusters.to_numpy()
 cluster_names=clusters[:,0]
@@ -33,33 +35,66 @@ for iname,name in enumerate(cluster_names):
     Clusters[name]={}
     Clusters[name]['LMEs']=lmes
     lims=[99999,-99999,99999,-99999]
+    X=np.array([])
+    Y=np.array([])
+
     for lme in lmes:
-        a=np.where(LME_mask==lme) 
+        a=np.where(LME_mask==lme)
+        X=np.append(X,a[1])
+        Y=np.append(Y,a[0])
 
-   
-        lims=np.array([np.min([lims[0],np.min(a[1])]),
-               np.max([lims[1],np.max(a[1])]),
-               np.min([lims[2],np.min(a[0])]),
-               np.max([lims[3],np.max(a[0])])    
-             ])
+ 
 
+
+    lims=np.array([np.min(X),np.max(X),np.min(Y),np.max(Y)])
+    if name == 'S Asia':
+        lims[0]=1280
+        lims[1]=122
     Clusters[name]['limits']=lims+[0,0,J_offset,J_offset]
+nname=iname+1
+  
+plt.pcolormesh(D[J_offset:,:])
+plt.ylabel('j-189')
+Lims=np.zeros((nname,4))
 
-plt.pcolormesh(D)
-for name in cluster_names:
+for iname,name in enumerate(cluster_names):
     lims=Clusters[name]['limits']
 
     imin = lims[0]
     imax = lims[1]
-    jmin = lims[2]
-    jmax = lims[3]
+    jmin = lims[2]-J_offset
+    jmax = lims[3]-J_offset
     if name !='S Asia':
       plt.plot([imin,imax,imax,imin,imin],[jmin,jmin,jmax,jmax,jmin])
     else:
       print(name)
-      plt.plot([imax, nx, np.nan, nx, imax, imax,np.nan,
-                0,imin,imin,0],
+      plt.plot([imin, nx, np.nan, nx, imin, imin,np.nan,
+                0,imax,imax,0],
                [jmin, jmin,np.nan, jmax, jmax, jmin,np.nan,
                 jmin,jmin,jmax,jmax])
+    Lims[iname,:]=lims
       
+LME_Data['DOMNAM'][63]=LME_Data['DOMNAM'][53]      
+PD={}
+PD['Cluster']=cluster_names
+PD['i min']=Lims[:,0]
+PD['i max']=Lims[:,1]
+PD['j min']=Lims[:,2]
+PD['j max']=Lims[:,3]
+
+for ilme in range(LME_numbers.shape[1]  ):
+    NN=[]
+    for iname,name in enumerate(cluster_names):     
+        try:
+            NN.append(LME_Data['DOMNAM'][int(LME_numbers[iname,ilme])-1])     
+        except:
+            NN.append('')
+    PD['LME {0}'.format(ilme+1)]=NN
+
+df=pd.DataFrame(PD)
+df.to_csv(LME_Clusters_out)    
+
+
+
+
 
