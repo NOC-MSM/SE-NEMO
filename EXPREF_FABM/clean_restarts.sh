a4b92d5bbea28cf7a6dd43b40460ab83b1b336c6
Yuri Artioli <yuri@pml.ac.uk>
2023-03-17 15:28:08 +0000

    tweaks to accessory files to handles both spin-up scycle and normal run

--- EXPREF_FABM/clean_restarts.sh
+++ EXPREF_FABM/clean_restarts.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# bash clean_restarts.sh $m0
+# bash clean_restarts.sh $m0 $nn_spin_cycle
 # deletes restarts from 2 months before
 # must be adapted to count also for the year
 
-#m0=$1
 ITER=$1
+SPIN=$2
 INDIR=$PWD
 
 echo cleaning restarts from previous two months, ITER: $ITER
 
-#mm=$(( m0-2 ))
-#if [ $mm2 -le 0 ]
-#then
-#  mm2=1
-#fi
-#mm2str=$(printf %02d $mm2)
-
-#source nn_itend_${mm2str} #reads ITERmm2 value
 SERIAL=$(printf "%08d" $ITER)
 
-rm $INDIR/RESTARTS/SENEMO_S?_${SERIAL}_restart_????.nc
-rm $INDIR/RESTARTS/SENEMO_S?_${SERIAL}_restart_ice_????.nc
-rm $INDIR/RESTARTS/SENEMO_S?_${SERIAL}_restart_trc_????.nc
-
-rm $INDIR/RESTARTS/SENEMO_${SERIAL}_restart_????.nc
-rm $INDIR/RESTARTS/SENEMO_${SERIAL}_restart_ice_????.nc
-rm $INDIR/RESTARTS/SENEMO_${SERIAL}_restart_trc_????.nc
-
+if [ $SPIN -ge 4 ] #NB, hardcoded 3y spin cycle
+then
+  rm $INDIR/RESTARTS/SENEMO_S?_${SERIAL}_restart_????.nc
+  rm $INDIR/RESTARTS/SENEMO_S?_${SERIAL}_restart_ice_????.nc
+  rm $INDIR/RESTARTS/SENEMO_S?_${SERIAL}_restart_trc_????.nc
+else
+  rm $INDIR/RESTARTS/SENEMO_${SERIAL}_restart_????.nc
+  rm $INDIR/RESTARTS/SENEMO_${SERIAL}_restart_ice_????.nc
+  rm $INDIR/RESTARTS/SENEMO_${SERIAL}_restart_trc_????.nc
+fi
