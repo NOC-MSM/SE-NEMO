aa5602318e24783ff4619a10d30d44c3f7972d12
jasontempestholt <42639421+jasontempestholt@users.noreply.github.com>
2024-01-29 12:59:47 +0000

    Code for stratification analysis on LME's

--- scripts/analysis/Python/EN4_profiles_LME.py
+++ scripts/analysis/Python/EN4_profiles_LME.py
@@ -23,30 +23,31 @@ coast_path='C:\\Users\\jholt\\Git\\COAsT'
 
 sys.path.insert(0,coast_path)
 import coast
-
+LME_Data=np.load('../Data/LME_gridinfo_V4.npz')
 en4_path='/projectsa/NEMO/OBS/EN4.2.1/'
 en4_name='EN.4.2.1.f.profiles.l09.'
 
 en4_path='C:\\Users\\jholt\\OneDrive - NOC\\Documents\\Data\\EN4\\'
 
-LME_Clusters='../Data/LME_Clusters_eORCA025.csv'
-clusters = pd.read_csv(LME_Clusters)
-nclusters=clusters.values.shape[0]
 fn_profile_config='../Config/example_en4_profiles.json'
 fn_domain='/projectsa/NEMO/jholt/SE-NEMO/INPUTS/domcfg_eORCA025_v2.nc'
 fn_domain='C:\\Users\\jholt\\OneDrive - NOC\\Documents\\Data\\SENEMO\\eORCA025_bathy_meter.nc'
 
 ystart = 2019
 ystop = 2019
+nLME = LME_Data["DOMNAM"].shape[0]
+for iLME in  range(67): # needs to work for cluster 17
+    LME_Name=LME_Data["DOMNAM"][iLME]
 
-for icluster in range(nclusters): # needs to work for cluster 17
     nemo = coast.Gridded(fn_data=fn_domain, config='example_nemo_grid_t.json')
     start_time = time.perf_counter()
-    print(clusters.values[icluster,1])
-    lonlims=clusters.values[icluster,6:8]
-    latlims=clusters.values[icluster,8:10]
-    x_min,x_max=lonlims
-    y_min,y_max=latlims
+    print(LME_Name)
+
+    x_min = LME_Data["x_min"][iLME]
+    x_max = LME_Data["x_max"][iLME]
+    y_min = LME_Data["y_min"][iLME]
+    y_max = LME_Data["y_max"][iLME]
+
     region_bounds = [x_min,x_max,y_min,y_max]
     j,i,_=nemo.find_j_i_list(lon=[x_min,x_max,x_max,x_min],lat=[y_min,y_min,y_max,y_max])
     if i[1] > i[0]:
@@ -63,7 +64,7 @@ for icluster in range(nclusters): # needs to work for cluster 17
     pa.calc_pea(profile,nemo, Zmax,rmax=25.0,limits=limits)
     pa.match_to_grid(nemo,rmax=25.0,limits=limits)#,grid_name="eorca025") # this over rights nemo
 
-    fname='../Data/{0}_EN4_PEA_SST_SSS_test.nc'.format(clusters.values[icluster,1].replace(' ','_'))
+    fname='../Data/{0}_EN4_PEA_SST_SSS_v1.nc'.format(LME_Name)
     pa.dataset.to_netcdf(fname)
     stop_time = time.perf_counter()
     print(stop_time - start_time)
