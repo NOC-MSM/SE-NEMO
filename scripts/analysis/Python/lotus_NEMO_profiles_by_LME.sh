c89b9be230df08184feb480b3492223880fdcb76
jpolton <jpolton@gmail.com>
2024-04-30 18:09:18 +0100

    loop over LME

---
+++ scripts/analysis/Python/lotus_NEMO_profiles_by_LME.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## sbatch -J Thailand lotus_NEMO_profiles_by_LME.sh 34
+#SBATCH --partition=short-serial
+#SBATCH --mem=40000
+#SBATCH -o LOGS/%A_%a.out
+#SBATCH -e LOGS/%A_%a.err
+#SBATCH --time=60:00
+#SBATCH --ntasks=1
+module add jaspy
+source activate senemo-profile-lme
+
+echo $1
+python  /gws/nopw/j04/class_vol2/senemo/jelt/PROCESSED/EN4.2.1/1978-2019/NEMO_profiles_by_LME.py $1 > LOGS/OUT_$1
